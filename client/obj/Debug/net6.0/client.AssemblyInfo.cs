886e2703c5a4048d55ddd9c7d9d7e2795cc7da61
DannyVroegop <danny436@hotmail.com>
2024-05-04 11:00:58 +0200

    hamlet removed client oops

--- client/obj/Debug/net6.0/client.AssemblyInfo.cs
+++ client/obj/Debug/net6.0/client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72b81521720cbf5ef6958c29f1ab91ef93929e7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6698a64689e0cd9b037ca3f86dbeae6f6735e16")]
 [assembly: System.Reflection.AssemblyProductAttribute("client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
